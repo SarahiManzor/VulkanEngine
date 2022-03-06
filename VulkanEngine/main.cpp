fe2997cfdbbe3f6df689020233a5ea3f43efdec3
manzorsarahi <manzorsarahi@gmail.com>
2022-03-06 11:57:12 -0500

    My first window

--- VulkanEngine/main.cpp
+++ VulkanEngine/main.cpp
@@ -1,28 +1,50 @@
-#include <vulkan/vulkan.h>
+#define GLFW_INCLUDE_VULKAN // This define is needed for glfw3.h to import vulkan/vulkan.h
+#include <GLFW/glfw3.h>
 
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
+#include <inttypes.h>
+
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
 
 class HelloTriangleApplication {
 public:
+private:
+	GLFWwindow* window;
+
+public:
 	void run() {
+		initWindow();
 		initVulkan();
 		mainLoop();
 		cleanup();
 	}
 
 private:
+	void initWindow() {
+		glfwInit();
+
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API); // Tells GLFW to not create an OpenGL context
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE); // Self explanatory
+
+		window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+	}
+
 	void initVulkan() {
 
 	}
 
 	void mainLoop() {
-
+		while (!glfwWindowShouldClose(window)) {
+			glfwPollEvents();
+		}
 	}
 
 	void cleanup() {
-
+		glfwDestroyWindow(window);
+		glfwTerminate();
 	}
 };
 
